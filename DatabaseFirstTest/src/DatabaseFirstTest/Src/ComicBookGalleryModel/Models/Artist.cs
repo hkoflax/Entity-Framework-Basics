b2ae4d9574d7c6a03cb363bb52d2137de345a6a6
hkoflax <hkoflax@users.noreply.github.com>
2019-04-23 13:00:53 -0400

    using Data Annotations to refine the generated database

--- DatabaseFirstTest/src/DatabaseFirstTest/Src/ComicBookGalleryModel/Models/Artist.cs
+++ DatabaseFirstTest/src/DatabaseFirstTest/Src/ComicBookGalleryModel/Models/Artist.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ComicBookGalleryModel.Models
 {
+    //to rename the table
+    //[Table("Talent")]
     public class Artist
     {
         public Artist()
@@ -13,8 +17,13 @@ namespace ComicBookGalleryModel.Models
             ComicBooks = new List<ComicBookArtist>();
         }
         public int Id { get; set; }
+        //To rename the column and set the string Lenght
+        //[Required, StringLength(100), Column("FullName")]
         public string Name { get; set; }
 
+        //To ignore any settable property by EF
+        //[NotMapped]
+        public string Test { get; set; }
         public ICollection<ComicBookArtist> ComicBooks { get; set; }
     }
 }
