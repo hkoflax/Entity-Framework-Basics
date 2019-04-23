d7648520a7cdd0ec1bfd87def6e890491596fdc6
hkoflax <hkoflax@users.noreply.github.com>
2019-04-23 16:25:56 -0400

    Overriding the Context's OnModelCreating Method
    Let's override the Context class' `OnModelCreating` method so that we can customize EF's conventions and use EF's fluent API to refine our model.

--- DatabaseFirstTest/src/DatabaseFirstTest/Src/ComicBookGalleryModel/Context.cs
+++ DatabaseFirstTest/src/DatabaseFirstTest/Src/ComicBookGalleryModel/Context.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,5 +27,17 @@ namespace ComicBookGalleryModel
             Database.SetInitializer(new DropCreateDatabaseAlways<Context>());
         }
         public DbSet<ComicBook> ComicBooks { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            //modelBuilder.Conventions.Remove<DecimalPropertyConvention>();
+            //modelBuilder.Conventions.Add(new DecimalPropertyConvention(5,2));
+
+            modelBuilder.Entity<ComicBook>()
+                .Property(cb => cb.Averagerating)
+                .HasPrecision(5,2);
+
+        }
     }
 }
